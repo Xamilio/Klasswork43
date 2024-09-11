d71065310cb3f0bd106c158273b47da87ec6c5e8
novaol55 <novaol55@33-8>
2024-09-11 11:36:00 +0300

    Add project files.

---
+++ Klasswork43/Klasswork43.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "  \"Hello World!\" \n\\Bjarne Stroustrup\\\n";
+
+
+	cout << "  Hello World!\n\\\\Denis Ritchi\\\\";
+
+	cout << "\n\nTo be, or not to be: that is the question:\nWhether 'tis nobler in the mind to suffer\nThe slings and arrows of outrageous fortune,\nOr to take arms against a sea of troubles,\nAnd by opposing end them?\nWilliam Shakespeare";
+}
+
